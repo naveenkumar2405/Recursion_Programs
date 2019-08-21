ccb920288441b4a787c8b6a23d0f6312e25fbf78
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-21 10:47:02 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)  
+{  
+    if (n==0)  
+    {  
+        return 0;  
+    }  
+    else if ( n == 1)  
+    {  
+        return 1;  
+    }  
+    else   
+    {  
+        return n*fact(n-1);  
+    }  
+} 
+ 
+int main()  
+{  
+    int n,f;  
+ 
+    cin>>n;  
+    f = fact(n);  
+    cout<<f;  
+} 
+
